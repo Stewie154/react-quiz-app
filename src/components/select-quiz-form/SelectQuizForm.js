5358517e3198251db0e9cfba392a563aacd04587
Stewart McFarlane <stewart@toru.digital>
2022-04-09 16:44:14 +0100

    added form field

--- src/components/select-quiz-form/SelectQuizForm.js
+++ src/components/select-quiz-form/SelectQuizForm.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const SelectQuizForm = () => {
 	return (
-		<div>SelectQuizForm</div>
+		<form>
+			<div className="form-group">
+				<label>Number of Questions</label>
+				<input 
+					type="number" 
+					name="no_of_questions" 
+					className="form-element" 
+					min="5" 
+					max="50"
+				/>
+			</div>
+		</form>
 	)
 }
 
