8d6d0c7eba7f75f7546b94c81235419c679a41d9
Stewart McFarlane <stewart@toru.digital>
2022-04-09 13:35:39 +0100

    created global stylesheet & indentation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.scss'
+import '../../styles/global.scss'
 
 const App = () => {
-  return (
-	<div className='app'>
-		Hello world
-	</div>
-  )
+	return (
+		<div className='app'>
+			Hello world
+		</div>
+	)
 }
 
 export default App
