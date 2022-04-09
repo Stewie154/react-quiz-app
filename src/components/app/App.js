238780b41a8676ef2ef3cee25f2a3b13240ca25b
Stewart McFarlane <stewart@toru.digital>
2022-04-09 16:22:56 +0100

    created SelectQuizForm component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import './App.scss'
 import '../../styles/global.scss'
 
 import WelcomeComponent from '../welcome-component/WelcomeComponent'
+import SelectQuizForm from '../select-quiz-form/SelectQuizForm'
 
 const App = () => {
 
@@ -19,7 +20,7 @@ const App = () => {
 			<h1 className="main-title">Quiz App</h1>
 			<div className='app-container'>
 				<WelcomeComponent  selectQuizClick={toggleQuizSelection}/>
-
+				<SelectQuizForm />
 			</div>
 		</div>
 		
---
+++ src/components/select-quiz-form/SelectQuizForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectQuizForm = () => {
+	return (
+		<div>SelectQuizForm</div>
+	)
+}
+
+export default SelectQuizForm
