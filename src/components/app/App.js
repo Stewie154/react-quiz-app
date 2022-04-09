ef4bf05f759071d76034aa2252387319785602d1
Stewart McFarlane <stewart@toru.digital>
2022-04-09 15:39:47 +0100

    created WelcomeComponent

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.scss'
 import '../../styles/global.scss'
 
+import WelcomeComponent from '../welcome-component/WelcomeComponent'
+
 const App = () => {
 	return (
 		<div className="wrapper">
 			<h1 className="main-title">Quiz App</h1>
 			<div className='app-container'>
-				
+				<WelcomeComponent />
 			</div>
 		</div>
 		
