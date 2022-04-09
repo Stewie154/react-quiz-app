9cb911b4cabb4714aab516b3b4f8bcda0134af78
Stewart McFarlane <stewart@toru.digital>
2022-04-09 15:47:51 +0100

    created ButtonComponent

--- src/components/welcome-component/WelcomeComponent.js
+++ src/components/welcome-component/WelcomeComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './WelcomeComponent.scss'
+import ButtonComponent from '../button-component/ButtonComponent'
 
 const WelcomeComponent = () => {
 	return (
 		<div className="welcome-component">
 			<h3 className="greeting-message">Welcome to the quiz!</h3>
-			
+			<ButtonComponent />
 		</div>
 	)
 }
