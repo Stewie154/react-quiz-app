45cf266ee30e4c6942f584f06c457c26ee0f3686
Stewart McFarlane <stewart@toru.digital>
2022-04-09 16:52:46 +0100

    added header & form flex layout

--- src/components/select-quiz-form/SelectQuizForm.js
+++ src/components/select-quiz-form/SelectQuizForm.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import './SelectQuizForm.scss'
 
 const SelectQuizForm = () => {
 	return (
-		<form>
-			<div className="form-group">
-				<label>Number of Questions</label>
-				<input 
-					type="number" 
-					name="no_of_questions" 
-					className="form-element" 
-					min="5" 
-					max="50"
-				/>
-			</div>
-		</form>
+		<>
+			<h3 className="header">Quiz Selection</h3>
+			<form>
+				<div className="form-group">
+					<label>Number of Questions</label>
+					<input 
+						type="number" 
+						name="no_of_questions" 
+						className="form-element" 
+						min="5" 
+						max="50"
+					/>
+				</div>
+			</form>
+		</>
+		
 	)
 }
 
