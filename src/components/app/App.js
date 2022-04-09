9b2f934226e448a5c551a7daf254b511361659fb
Stewart McFarlane <stewart@toru.digital>
2022-04-09 15:17:14 +0100

    wrapped container in wrapper to add main title

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,9 +4,13 @@ import '../../styles/global.scss'
 
 const App = () => {
 	return (
-		<div className='app-container'>
-			Hello world
+		<div className="wrapper">
+			<h1 className="main-title">Quiz App</h1>
+			<div className='app-container'>
+				app here
+			</div>
 		</div>
+		
 	)
 }
 
