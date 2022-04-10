5715a751d1df922cec9902bea78b81a39f06ea28
Stewart McFarlane <stewart@toru.digital>
2022-04-10 10:50:11 +0100

    added category select

--- src/components/select-quiz-form/SelectQuizForm.js
+++ src/components/select-quiz-form/SelectQuizForm.js
@@ -17,14 +17,37 @@ const SelectQuizForm = () => {
 					/>
 				</div>
 				<div className="form-group">
-					<label>Number of Questions:</label>
-					<input 
+					<label>Select Category</label>
+					<select 
 						className="form-element" 
-						type="number" 
-						name="no_of_questions" 
-						min="5" 
-						max="50"
-					/>
+						name="trivia_category" 
+					>
+						<option value="any">Any Category</option>
+						<option value="9">General Knowledge</option>
+						<option value="10">Entertainment: Books</option>
+						<option value="11">Entertainment: Film</option>
+						<option value="12">Entertainment: Music</option>
+						<option value="13">Entertainment: Musicals &amp; Theatres</option>
+						<option value="14">Entertainment: Television</option>
+						<option value="15">Entertainment: Video Games</option>
+						<option value="16">Entertainment: Board Games</option>
+						<option value="17">Science &amp; Nature</option>
+						<option value="18">Science: Computers</option>
+						<option value="19">Science: Mathematics</option>
+						<option value="20">Mythology</option>
+						<option value="21">Sports</option>
+						<option value="22">Geography</option>
+						<option value="23">History</option>
+						<option value="24">Politics</option>
+						<option value="25">Art</option>
+						<option value="26">Celebrities</option>
+						<option value="27">Animals</option>
+						<option value="28">Vehicles</option>
+						<option value="29">Entertainment: Comics</option>
+						<option value="30">Science: Gadgets</option>
+						<option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+						<option value="32">Entertainment: Cartoon &amp; Animations</option>
+					</select>
 				</div>
 			</form>
 		</>
