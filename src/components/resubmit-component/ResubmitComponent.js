2f80d3e921f2b5db3039d1c944702d7adb06a730
Stewart McFarlane <stewart@toru.digital>
2022-04-10 15:53:01 +0100

    created ResubmitComponent

---
+++ src/components/resubmit-component/ResubmitComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ResubmitComponent.scss'
+
+const ResubmitComponent = () => {
+  return (
+    <div>ResubmitComponent</div>
+  )
+}
+
+export default ResubmitComponent
