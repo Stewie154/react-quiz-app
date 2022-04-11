46a4c382a0d3ce8bd3c9dae3f1766407bb85f0bf
Stewart McFarlane <stewart@toru.digital>
2022-04-11 19:51:50 +0100

    added backup questions

--- src/api/backupQuestions.js
+++ src/api/backupQuestions.js
@@ -1,1 +1,604 @@
-const backupQuestions = []
+const backupQuestions = [
+	{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "According to the BBPA, what is the most common pub name in the UK?",
+		"correct_answer": "Red Lion",
+		"incorrect_answers": [
+		"Royal Oak",
+		"White Hart",
+		"King&#039;s Head"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "This field is sometimes known as &ldquo;The Dismal Science.&rdquo;",
+		"correct_answer": "Economics",
+		"incorrect_answers": [
+		"Philosophy",
+		"Politics",
+		"Physics"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "In a standard set of playing cards, which is the only king without a moustache?",
+		"correct_answer": "Hearts",
+		"incorrect_answers": [
+		"Spades",
+		"Diamonds",
+		"Clubs"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the defining characteristic of someone who is described as hirsute?",
+		"correct_answer": "Hairy",
+		"incorrect_answers": [
+		"Rude",
+		"Funny",
+		"Tall"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Rolex is a company that specializes in what type of product?",
+		"correct_answer": "Watches",
+		"incorrect_answers": [
+		"Cars",
+		"Computers",
+		"Sports equipment"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Scotch whisky and Drambuie make up which cocktail?",
+		"correct_answer": "Rusty Nail",
+		"incorrect_answers": [
+		"Screwdriver",
+		"Sex on the Beach",
+		"Manhattan"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What does the &quot;G&quot; mean in &quot;G-Man&quot;?",
+		"correct_answer": "Government",
+		"incorrect_answers": [
+		"Going",
+		"Ghost",
+		"Geronimo"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which language is NOT Indo-European?",
+		"correct_answer": "Hungarian",
+		"incorrect_answers": [
+		"Russian",
+		"Greek",
+		"Latvian"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which essential condiment is also known as Japanese horseradish?",
+		"correct_answer": "Wasabi ",
+		"incorrect_answers": [
+		"Mentsuyu",
+		"Karashi",
+		"Ponzu"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which of the following Ivy League universities has its official motto in Hebrew as well as in Latin?",
+		"correct_answer": "Yale University",
+		"incorrect_answers": [
+		"Princeton University",
+		"Harvard University",
+		"Columbia University"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Who is a co-founder of music streaming service Spotify?",
+		"correct_answer": "Daniel Ek",
+		"incorrect_answers": [
+		"Sean Parker",
+		"Felix Miller",
+		"Michael Breidenbruecker"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What was the destination of the missing flight MH370?",
+		"correct_answer": "Beijing",
+		"incorrect_answers": [
+		"Kuala Lumpur",
+		"Singapore",
+		"Tokyo"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the unit of currency in Laos?",
+		"correct_answer": "Kip",
+		"incorrect_answers": [
+		"Ruble",
+		"Konra",
+		"Dollar"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What did the Spanish autonomous community of Catalonia ban in 2010, that took effect in 2012?",
+		"correct_answer": "Bullfighting",
+		"incorrect_answers": [
+		"Fiestas",
+		"Flamenco",
+		"Mariachi"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which Italian automobile manufacturer gained majority control of U.S. automobile manufacturer Chrysler in 2011?",
+		"correct_answer": "Fiat",
+		"incorrect_answers": [
+		"Maserati",
+		"Alfa Romeo",
+		"Ferrari"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the Swedish word for &quot;window&quot;?",
+		"correct_answer": "F&ouml;nster",
+		"incorrect_answers": [
+		"H&aring;l",
+		"Sk&auml;rm",
+		"Ruta"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the Italian word for &quot;tomato&quot;?",
+		"correct_answer": "Pomodoro",
+		"incorrect_answers": [
+		"Aglio",
+		"Cipolla",
+		"Peperoncino"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the romanized Russian word for &quot;winter&quot;?",
+		"correct_answer": "Zima",
+		"incorrect_answers": [
+		"Leto",
+		"Vesna",
+		"Osen&#039;"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the name of the popular animatronic singing fish prop, singing such hits such as &quot;Don&#039;t Worry, Be Happy&quot;?",
+		"correct_answer": "Big Mouth Billy Bass",
+		"incorrect_answers": [
+		"Big Billy Bass",
+		"Singing Fish",
+		"Sardeen"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What country saw a world record 315 million voters turn out for elections on May 20, 1991?",
+		"correct_answer": "India",
+		"incorrect_answers": [
+		"United States of America",
+		"Soviet Union",
+		"Poland"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "On average, Americans consume 100 pounds of what per second?",
+		"correct_answer": "Chocolate",
+		"incorrect_answers": [
+		"Potatoes",
+		"Donuts",
+		"Cocaine"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Who is the founder of &quot;The Lego Group&quot;?",
+		"correct_answer": "Ole Kirk Christiansen",
+		"incorrect_answers": [
+		" Jens Niels Christiansen",
+		"Kirstine Christiansen",
+		" Gerhardt Kirk Christiansen"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is a dead mall?",
+		"correct_answer": "A mall with high vacancy rates or low consumer foot traffic",
+		"incorrect_answers": [
+		"A mall with no stores",
+		"A mall that has been condemed",
+		"A mall after business hours"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "The new One World Trade Center in Manhattan, New York City was designed by which architect? ",
+		"correct_answer": "David Childs",
+		"incorrect_answers": [
+		"Bjarke Ingels",
+		"Michael Arad",
+		"Fumihiko Maki"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What year was Apple Inc. founded?",
+		"correct_answer": "1976",
+		"incorrect_answers": [
+		"1978",
+		"1980",
+		"1974"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "In what year was McDonald&#039;s founded?",
+		"correct_answer": "1955",
+		"incorrect_answers": [
+		"1964",
+		"1951",
+		"1947"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which of the General Mills Corporation&#039;s monster cereals was the last to be released in the 1970&#039;s?",
+		"correct_answer": "Fruit Brute",
+		"incorrect_answers": [
+		"Count Chocula",
+		"Franken Berry",
+		"Boo-Berry"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What was Mountain Dew&#039;s original slogan?",
+		"correct_answer": "Yahoo! Mountain Dew... It&#039;ll tickle your innards!",
+		"incorrect_answers": [
+		"Give Me A Dew",
+		"Do The Dew",
+		"Get&#039; that barefoot feelin&#039; drinkin&#039; Mountain Dew"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which iconic Disneyland attraction was closed in 2017 to be remodeled as a &quot;Guardians of the Galaxy&quot; themed ride?",
+		"correct_answer": "Twilight Zone Tower of Terror",
+		"incorrect_answers": [
+		"The Haunted Mansion",
+		"Pirates of the Caribbean",
+		"Peter Pan&#039;s Flight"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is a Burgee?",
+		"correct_answer": "A flag",
+		"incorrect_answers": [
+		"A rope",
+		"A window",
+		"A type of food"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
+		"correct_answer": "Jumping over an office chair",
+		"incorrect_answers": [
+		"Jumping backwards over a desk",
+		"Standing on his head",
+		"Typing on a keyboard during a handstand"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Apple co-founder Steve Jobs died from complications of which form of cancer?",
+		"correct_answer": "Pancreatic",
+		"incorrect_answers": [
+		"Bone",
+		"Liver",
+		"Stomach"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Computer manufacturer Compaq was acquired for $25 billion dollars in 2002 by which company?",
+		"correct_answer": "Hewlett-Packard",
+		"incorrect_answers": [
+		"Toshiba",
+		"Asus",
+		"Dell"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is real haggis made of?",
+		"correct_answer": "Sheep&#039;s Heart, Liver and Lungs",
+		"incorrect_answers": [
+		"Sheep&#039;s Heart, Kidneys and Lungs",
+		"Sheep&#039;s Liver, Kidneys and Eyes",
+		"Whole Sheep"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What was the original name of the search engine &quot;Google&quot;?",
+		"correct_answer": "BackRub",
+		"incorrect_answers": [
+		"CatMassage",
+		"SearchPro",
+		"Netscape Navigator"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Earl Grey tea is black tea flavoured with what?",
+		"correct_answer": "Bergamot oil",
+		"incorrect_answers": [
+		"Lavender",
+		"Vanilla",
+		"Honey"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Whose greyscale face is on the kappa emoticon on Twitch?",
+		"correct_answer": "Josh DeSeno",
+		"incorrect_answers": [
+		"Justin DeSeno",
+		"John DeSeno",
+		"Jimmy DeSeno"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which item of clothing is usually worn by a Scotsman at a wedding?",
+		"correct_answer": "Kilt",
+		"incorrect_answers": [
+		"Skirt",
+		"Dress",
+		"Rhobes"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which country drives on the left side of the road?",
+		"correct_answer": "Japan",
+		"incorrect_answers": [
+		"Germany",
+		"Russia",
+		"China"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the currency of Poland?",
+		"correct_answer": "Złoty",
+		"incorrect_answers": [
+		"Ruble",
+		"Euro",
+		"Krone"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the star sign of someone born on Valentines day?",
+		"correct_answer": "Aquarius",
+		"incorrect_answers": [
+		"Pisces",
+		"Capricorn",
+		"Scorpio"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "In ancient Greece, if your job were a &quot;hippeus&quot; which of these would you own?",
+		"correct_answer": "Horse",
+		"incorrect_answers": [
+		"Weave",
+		"Guitar",
+		"Boat"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What is the full title of the Prime Minister of the UK?",
+		"correct_answer": "First Lord of the Treasury",
+		"incorrect_answers": [
+		"Duke of Cambridge",
+		"Her Majesty&#039;s Loyal Opposition",
+		"Manager of the Crown Estate"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Directly between the Washington Monument and the Reflecting Pool is a memorial to which war?",
+		"correct_answer": "World War II",
+		"incorrect_answers": [
+		"Vietnam War",
+		"American Civil War",
+		"American Revolutionary War"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "The Mexican Beer &quot;Corona&quot; is what type of beer?",
+		"correct_answer": "Pale Lager",
+		"incorrect_answers": [
+		"India Pale Ale",
+		"Pilfsner",
+		"Baltic Porter"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Which logical fallacy means to attack the character of your opponent rather than their arguments?",
+		"correct_answer": "Ad hominem",
+		"incorrect_answers": [
+		"Post hoc ergo propter hoc",
+		"Tu quoque",
+		"Argumentum ad populum"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "The architect known as Le Corbusier was an important figure in what style of architecture?",
+		"correct_answer": "Modernism",
+		"incorrect_answers": [
+		"Neoclassical",
+		"Baroque",
+		"Gothic Revival"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "Frank Lloyd Wright was the architect behind what famous building?",
+		"correct_answer": "The Guggenheim",
+		"incorrect_answers": [
+		"Villa Savoye",
+		"Sydney Opera House",
+		"The Space Needle"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What fast food chain has the most locations globally? ",
+		"correct_answer": "Subway",
+		"incorrect_answers": [
+		"Starbucks",
+		"McDonalds",
+		"KFC"
+		]
+		},
+		{
+		"category": "General Knowledge",
+		"type": "multiple",
+		"difficulty": "medium",
+		"question": "What are the three starter Pokemon available in Pokemon Black and White?",
+		"correct_answer": "Snivy, Tepig, Oshawott",
+		"incorrect_answers": [
+		"Snivy, Fennekin, Froakie",
+		"Chespin, Tepig, Froakie",
+		"Chespin, Fennekin, Oshawott"
+		]
+		}
+]
+
+export default backupQuestions
