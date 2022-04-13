790176f63bd5a392533b2029ddc42a52964669db
Stewart McFarlane <stewart@toru.digital>
2022-04-13 20:29:16 +0100

    handleSearchAgain to take user back to the form component

--- src/components/resubmit-component/ResubmitComponent.js
+++ src/components/resubmit-component/ResubmitComponent.js
@@ -4,11 +4,17 @@ import './ResubmitComponent.scss'
 import ButtonComponent from '../button-component/ButtonComponent'
 
 const ResubmitComponent = (props) => {
+
+	const handleSearchAgain = () => {
+		props.toggleQuizSelection()
+		props.toggleUserResubmitting()
+	}
+
 	return (
 		<div className="resubmit-component">
 			<h1>Failed to retrieve enough questions with your search criteria.</h1>
 			<div className="actions-container">
-				<ButtonComponent text="Search again"/>
+				<ButtonComponent text="Search again" handleClick={handleSearchAgain}/>
 				<ButtonComponent text="Play random quiz"/>
 			</div>
 		</div>
