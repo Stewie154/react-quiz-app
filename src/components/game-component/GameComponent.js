8ae2552212537240c95aa13223ebea674fd93bd2
Stewart McFarlane <stewart@toru.digital>
2022-04-17 17:24:05 +0100

    import ButtonComponent & render incorrect answers

--- src/components/game-component/GameComponent.js
+++ src/components/game-component/GameComponent.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import './GameComponent.scss'
 
+import ButtonComponent from '../button-component/ButtonComponent'
+
 const GameComponent = (props) => {
 
 	const [currentQuestion, setCurrentQuestion] = useState(props.questions[0])
 
 	console.log(currentQuestion)
 
+	const answers = currentQuestion.incorrect_answers.map((answer) => <ButtonComponent text={answer}/>)
+
 return (
 	<div className="game-component">
 		<h1 className="question">{currentQuestion.question}</h1>
+		<main className="answers-grid">
+			{answers}
+		</main>
 	</div>
 )
 }
