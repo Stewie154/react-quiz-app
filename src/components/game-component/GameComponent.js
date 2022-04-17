cc3cb3c48d9742b386486bc9397f89bd79b3a282
Stewart McFarlane <stewart@toru.digital>
2022-04-17 17:35:21 +0100

    CurrentQuestionIndex state & render LoadingSpinner when no question is set

--- src/components/game-component/GameComponent.js
+++ src/components/game-component/GameComponent.js
@@ -2,18 +2,26 @@ import React, { useState, useEffect } from 'react'
 import './GameComponent.scss'
 
 import ButtonComponent from '../button-component/ButtonComponent'
+import LoadingSpinner from '../loading-spinner/LoadingSpinner'
 
 const GameComponent = (props) => {
 
-	const [currentQuestion, setCurrentQuestion] = useState(props.questions[0])
+	const [currentQuestion, setCurrentQuestion] = useState()
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+
+	useEffect(() => {
+		setCurrentQuestion(props.questions[0])
+	},[currentQuestion, currentQuestionIndex])
 
 	console.log(currentQuestion)
 
-	const answers = currentQuestion.incorrect_answers.map((answer) => <ButtonComponent text={answer}/>)
+	const answers = currentQuestion && currentQuestion.incorrect_answers.map((answer) => <ButtonComponent text={answer}/>)
 
 return (
 	<div className="game-component">
-		<h1 className="question">{currentQuestion.question}</h1>
+		{!currentQuestion && <LoadingSpinner />}
+		<h1></h1>
+		<h1 className="question">{currentQuestion && currentQuestion.question}</h1>
 		<main className="answers-grid">
 			{answers}
 		</main>
