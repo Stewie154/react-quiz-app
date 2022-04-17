109baa61526a6540b911d72241c7c9f12b1b55c5
Stewart McFarlane <stewart@toru.digital>
2022-04-17 17:41:48 +0100

    updateQuestionIndex function, tested on currently rendered buttons

--- src/components/game-component/GameComponent.js
+++ src/components/game-component/GameComponent.js
@@ -7,15 +7,22 @@ import LoadingSpinner from '../loading-spinner/LoadingSpinner'
 const GameComponent = (props) => {
 
 	const [currentQuestion, setCurrentQuestion] = useState()
-	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+	const [questionIndex, setQuestionIndex] = useState(0)
 
 	useEffect(() => {
 		setCurrentQuestion(props.questions[0])
-	},[currentQuestion, currentQuestionIndex])
+	},[currentQuestion, questionIndex])
+
+	const updateQuestionIndex = () => {
+		if(questionIndex < props.questions.length) {
+			setQuestionIndex(questionIndex + 1)
+		}
+		console.log(questionIndex)
+	}
 
 	console.log(currentQuestion)
 
-	const answers = currentQuestion && currentQuestion.incorrect_answers.map((answer) => <ButtonComponent text={answer}/>)
+	const answers = currentQuestion && currentQuestion.incorrect_answers.map((answer) => <ButtonComponent text={answer} handleClick={updateQuestionIndex}/>)
 
 return (
 	<div className="game-component">
