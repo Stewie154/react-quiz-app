2dce353207d437da379b1dea1ea7020cb4bbee85
Stewart McFarlane <stewart@toru.digital>
2022-04-21 20:36:04 +0100

    EndOfGameComponent flex container

--- src/components/end-of-game-component/EndOfGameComponent.js
+++ src/components/end-of-game-component/EndOfGameComponent.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './EndOfGameComponent.scss'
 
 const EndOfGameComponent = ({score, questionsLength}) => {
-  return (
-    <div>You scored {score} / {questionsLength}</div>
-  )
+	return (
+		<div className="end-of-game-component">
+			<h1>You scored {score} / {questionsLength}</h1>
+		</div>
+	)
 }
 
 export default EndOfGameComponent
