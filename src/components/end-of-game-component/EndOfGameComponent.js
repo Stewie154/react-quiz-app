67e05af741bb96b9dd5c26523fcf191e1701d274
Stewart McFarlane <stewart@toru.digital>
2022-04-21 21:02:11 +0100

    added gifs and started getImage function

--- src/components/end-of-game-component/EndOfGameComponent.js
+++ src/components/end-of-game-component/EndOfGameComponent.js
@@ -2,9 +2,31 @@ import React from 'react'
 import './EndOfGameComponent.scss'
 
 const EndOfGameComponent = ({score, questionsLength}) => {
+	const images = {
+		winner: [
+			'images/winner.gif',
+			'images/good.gif',
+		],
+		notBad: [
+			'not-bad.gif',
+			'not-bad-2.gif'
+		],
+		loser: [
+			'loser-2.gif',
+			'loser.gif'
+		]
+	}
+
+	const getImage = (score, questionsLength) => {
+		let percentage = score / questionsLength * 100
+		console.log(percentage)
+	}
+	getImage(score, questionsLength)
+
 	return (
 		<div className="end-of-game-component">
 			<h1>You scored {score} / {questionsLength}</h1>
+			<img src='images/winner.gif'/>
 		</div>
 	)
 }
