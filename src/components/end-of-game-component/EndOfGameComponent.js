2afdbc9fb401cbf6b27ca56d73a1d2b7c223c2f4
Stewart McFarlane <stewart@toru.digital>
2022-04-23 17:49:39 +0100

    return correct image & message depending on score

--- src/components/end-of-game-component/EndOfGameComponent.js
+++ src/components/end-of-game-component/EndOfGameComponent.js
@@ -8,25 +8,46 @@ const EndOfGameComponent = ({score, questionsLength}) => {
 			'images/good.gif',
 		],
 		notBad: [
-			'not-bad.gif',
-			'not-bad-2.gif'
+			'images/not-bad.gif',
+			'images/not-bad-2.gif'
 		],
 		loser: [
-			'loser-2.gif',
-			'loser.gif'
+			'images/loser-2.gif',
+			'images/loser.gif'
 		]
 	}
 
-	const getImage = (score, questionsLength) => {
+	const getResult = (score, questionsLength) => {
 		let percentage = score / questionsLength * 100
+		let result = {
+			image: '',
+			text: ''
+		}
 		console.log(percentage)
+		if (percentage >= 80) {
+			result.image =  images.winner[Math.floor(Math.random() * images.winner.length)]
+			result.text = 'Fantastic!'
+		}
+		else if (percentage >= 50) {
+			result.image = images.notBad[Math.floor(Math.random() * images.notBad.length)]
+			result.text = 'Not Bad...'
+		}
+		else {
+			result.image = images.loser[Math.floor(Math.random() * images.loser.length)]
+			result.text = 'Um...thanks for playing!'
+		}
+		console.log(result.image + 'd')
+		return result
 	}
-	getImage(score, questionsLength)
+
+	
+	getResult(score, questionsLength)
 
 	return (
 		<div className="end-of-game-component">
 			<h1>You scored {score} / {questionsLength}</h1>
-			<img src='images/winner.gif'/>
+			<img className="result-image" src={getResult(score, questionsLength).image} />
+			<h3>{getResult(score, questionsLength).text}</h3>
 		</div>
 	)
 }
