4623da1bc29a8268d9fcfbc0b9a764f0b36dc87f
Stewart McFarlane <stewart@toru.digital>
2022-04-26 21:50:46 +0100

    fade animation on WelcomeComponent

--- src/components/welcome-component/WelcomeComponent.js
+++ src/components/welcome-component/WelcomeComponent.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './WelcomeComponent.scss'
 import ButtonComponent from '../button-component/ButtonComponent'
+import Fade from 'react-reveal/Fade';
+
 
 const WelcomeComponent = (props) => {
 	return (
-		<div className="welcome-component">
-			<h3 className="greeting-message">Welcome!</h3>
-			<div className="main-btn-container">
-				<ButtonComponent text="Select Quiz" handleClick={props.selectQuizClick}/>
+		<Fade up delay={100}>
+			<div className="welcome-component">
+				<h3 className="greeting-message">Welcome!</h3>
+				<div className="main-btn-container">
+					<ButtonComponent text="Select Quiz" handleClick={props.selectQuizClick}/>
+				</div>
 			</div>
-		</div>
+		</Fade>
 	)
 }
 
